ae0d911376dd15519415b22bbd6da6abe24a7d7f
MorbidArk <1482506841@qq.com>
2022-07-26 03:20:47 +0800

    修改代码结构

--- chip_8.cpp
+++ chip_8.cpp
@@ -68,199 +68,198 @@ void chip_8::init() {
  * CHIP-8 解释器模拟周期
  */
 void chip_8::emulateCycle() {
-    //获取操作码
+    // Fetch opcode
     opcode = memory[pc] << 8 | memory[pc + 1];
-    //解析操作码
-    switch (opcode & 0xF000) {
-        case 0x0000: // 00NN 系列操作码
-            switch (opcode & 0x00FF) {
-                case 0x00E0: // 0x00E0: 清除屏幕
-                    for (unsigned char &i: gfx)
-                        i = 0x0;
-                    drawFlag = true;
 
+    // Process opcode
+    switch (opcode & 0xF000) {
+        case 0x0000:
+            switch (opcode & 0x000F) {
+                case 0x0000: // 0x00E0: Clears the screen
+                    for (int i = 0; i < 2048; ++i)
+                        gfx[i] = 0x0;
+                    drawFlag = true;
                     pc += 2;
                     break;
 
-                case 0x00EE: // 0x00EE: 从子程序返回
-                    --sp;            // 16层堆栈，下移堆栈指针，以防止覆盖
-                    pc = stack[sp];    // 将堆栈中存储的返回地址放进程序计数器
-                    pc += 2;
+                case 0x000E: // 0x00EE: Returns from subroutine
+                    --sp;			// 16 levels of stack, decrease stack pointer to prevent overwrite
+                    pc = stack[sp];	// Put the stored return address from the stack back into the program counter
+                    pc += 2;		// Don't forget to increase the program counter!
                     break;
 
                 default:
-                    printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
+                    printf ("Unknown opcode [0x0000]: 0x%X\n", opcode);
             }
+            break;
 
-        case 0x1000: // 1NNN: 跳转到地址 NNN
+        case 0x1000: // 0x1NNN: Jumps to address NNN
             pc = opcode & 0x0FFF;
             break;
 
-        case 0x2000: // 2NNN: 在 NNN 调用子程序
-            stack[sp] = pc;
-            ++sp;
-            pc = opcode & 0x0FFF;
+        case 0x2000: // 0x2NNN: Calls subroutine at NNN.
+            stack[sp] = pc;			// Store current address in stack
+            ++sp;					// Increment stack pointer
+            pc = opcode & 0x0FFF;	// Set the program counter to the address at NNN
             break;
 
-        case 0x3000: // 3XNN: 如果 VX 等于 NN，则跳过下一条指令
+        case 0x3000: // 0x3XNN: Skips the next instruction if VX equals NN
             if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
                 pc += 4;
             else
                 pc += 2;
             break;
 
-        case 0x4000: // 0x4XNN: 如果 VX 不等于 NN，则跳过下一条指令
+        case 0x4000: // 0x4XNN: Skips the next instruction if VX doesn't equal NN
             if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
                 pc += 4;
             else
                 pc += 2;
             break;
 
-        case 0x5000: // 0x5XY0: 如果 VX 等于 VY，则跳过下一条指令
+        case 0x5000: // 0x5XY0: Skips the next instruction if VX equals VY.
             if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4])
                 pc += 4;
             else
                 pc += 2;
             break;
 
-        case 0x6000: // 0x6VNN: 将 VX 设置为 NN
-            V[(opcode & 0xF00) >> 8] = opcode & 0x00FF;
+        case 0x6000: // 0x6XNN: Sets VX to NN.
+            V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
             pc += 2;
             break;
 
-        case 0x7000: // 0x7XNN: 将 NN 添加到 VX (进位标志不变)
+        case 0x7000: // 0x7XNN: Adds NN to VX.
             V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
             pc += 2;
             break;
 
-        case 0x8000: // 8NNN 系列操作码
+        case 0x8000:
             switch (opcode & 0x000F) {
-                case 0x0000: // 0x8XY0: 将 VX 设置为 VY 的值
+                case 0x0000: // 0x8XY0: Sets VX to the value of VY
                     V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
                     pc += 2;
                     break;
 
-                case 0x0001: // 0x8XY1: 将 VX 设置为 ( VX | VY ) 的值
+                case 0x0001: // 0x8XY1: Sets VX to "VX OR VY"
                     V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
                     pc += 2;
                     break;
 
-                case 0x0002: // 0x8XY2: 将 VX 设置为 ( VX & VY ) 的值
+                case 0x0002: // 0x8XY2: Sets VX to "VX AND VY"
                     V[(opcode & 0x0F00) >> 8] &= V[(opcode & 0x00F0) >> 4];
                     pc += 2;
                     break;
 
-                case 0x0003: // 0x8XY3: 将 VX 设置为 ( VX ^ VY ) 的值
+                case 0x0003: // 0x8XY3: Sets VX to "VX XOR VY"
                     V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];
                     pc += 2;
                     break;
 
-                case 0x0004: // 0x8XY4: 将 VY 添加到 VX , VF 有进位时设为 1 ,没有进位时设为 0
+                case 0x0004: // 0x8XY4: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't
                     if (V[(opcode & 0x00F0) >> 4] > (0xFF - V[(opcode & 0x0F00) >> 8]))
-                        V[0xF] = 1;
+                        V[0xF] = 1; //carry
                     else
                         V[0xF] = 0;
                     V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
                     pc += 2;
                     break;
 
-                case 0x0005: // 0x8XY5: 将 VY 减去 VX , VF 有进位时设为 1 ,没有进位时设为 0
+                case 0x0005: // 0x8XY5: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't
                     if (V[(opcode & 0x00F0) >> 4] > V[(opcode & 0x0F00) >> 8])
-                        V[0xF] = 0;
+                        V[0xF] = 0; // there is a borrow
                     else
                         V[0xF] = 1;
                     V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
                     pc += 2;
                     break;
 
-                case 0x0006: // 0x8XY6: 将 VX 的最高有效位存储在 VF 中,然后将 VX 向左移动 1
+                case 0x0006: // 0x8XY6: Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift
                     V[0xF] = V[(opcode & 0x0F00) >> 8] & 0x1;
                     V[(opcode & 0x0F00) >> 8] >>= 1;
                     pc += 2;
                     break;
 
-                case 0x0007: // 0x8XY7: 将 VX 设置为 VY 减去 VX , VF 有借位时设为 0,没有借位时设为 1
-                    if (V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4])    // VY-VX
-                        V[0xF] = 0;
+                case 0x0007: // 0x8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+                    if (V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4])	// VY-VX
+                        V[0xF] = 0; // there is a borrow
                     else
                         V[0xF] = 1;
                     V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
                     pc += 2;
                     break;
 
-                case 0x000E: // 0x8XYE: 将 VX 的最高有效位存储在 VF 中,然后将 VX 向左移动 1
+                case 0x000E: // 0x8XYE: Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift
                     V[0xF] = V[(opcode & 0x0F00) >> 8] >> 7;
                     V[(opcode & 0x0F00) >> 8] <<= 1;
                     pc += 2;
                     break;
 
                 default:
-                    printf("Unknown opcode [0x8000]: 0x%X\n", opcode);
+                    printf ("Unknown opcode [0x8000]: 0x%X\n", opcode);
             }
             break;
-        case 0x9000: // 0x9XY0: 如果 VX 不等于 VY，则跳过下一条指令
+
+        case 0x9000: // 0x9XY0: Skips the next instruction if VX doesn't equal VY
             if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4])
                 pc += 4;
             else
                 pc += 2;
             break;
 
-        case 0xA000: // ANNN: 将 I 设置为地址 NNN
+        case 0xA000: // ANNN: Sets I to the address NNN
             I = opcode & 0x0FFF;
             pc += 2;
             break;
 
-        case 0xB000: // BNNN: 跳转到地址 NNN + V0
+        case 0xB000: // BNNN: Jumps to the address NNN plus V0
             pc = (opcode & 0x0FFF) + V[0];
             break;
 
-        case 0xC000: // CXNN: 将 VX 设置为对随机数(0~255)和 NN 进行按位和运算的结果
+        case 0xC000: // CXNN: Sets VX to a random number and NN
             V[(opcode & 0x0F00) >> 8] = (rand() % 0xFF) & (opcode & 0x00FF);
             pc += 2;
             break;
 
-        case 0xD000: // DXYN: 在坐标 (VX, VY) 处绘制一个宽度为 8 像素，高度为 N 像素的精灵
-            /**
-             * 从内存位置 I 开始，每行 8 个像素被读取为位编码
-             * I 值在执行该指令后不会改变
-             * 如果任何屏幕像素在绘制精灵时从设置翻转为未设置，则 VF 设置为 1
-             * 如果没有发生，则设置为 0
-             */
+        case 0xD000: // DXYN: Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
+            // Each row of 8 pixels is read as bit-coded starting from memory location I;
+            // I value doesn't change after the execution of this instruction.
+            // VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn,
+            // and to 0 if that doesn't happen
         {
-            // X , Y , H
             unsigned short x = V[(opcode & 0x0F00) >> 8];
             unsigned short y = V[(opcode & 0x00F0) >> 4];
             unsigned short height = opcode & 0x000F;
-
-            unsigned short pixel;   //8位像素编码
+            unsigned short pixel;
 
             V[0xF] = 0;
             for (int yline = 0; yline < height; yline++) {
                 pixel = memory[I + yline];
                 for (int xline = 0; xline < 8; xline++) {
-                    if ((pixel & (0x80 >> xline)) != 0) { // 判断 pixel 的值是否符合要求
-                        if (gfx[(x + xline + ((y + yline) * 64))] == 1) { // 检查(x,y)处的像素是否为 1
+                    if ((pixel & (0x80 >> xline)) != 0) {
+                        if (gfx[(x + xline + ((y + yline) * 64))] == 1) {
                             V[0xF] = 1;
                         }
                         gfx[x + xline + ((y + yline) * 64)] ^= 1;
                     }
                 }
             }
-            drawFlag = true;    //更新屏幕
+
+            drawFlag = true;
             pc += 2;
         }
             break;
 
         case 0xE000:
             switch (opcode & 0x00FF) {
-                case 0x009E: // EX9E: 如果按下存储在 VX 中的键,则跳过下一条指令
+                case 0x009E: // EX9E: Skips the next instruction if the key stored in VX is pressed
                     if (key[V[(opcode & 0x0F00) >> 8]] != 0)
                         pc += 4;
                     else
                         pc += 2;
                     break;
 
-                case 0x00A1: // EXA1: 如果未按下 VX 中存储的键,则跳过下一条指令
+                case 0x00A1: // EXA1: Skips the next instruction if the key stored in VX isn't pressed
                     if (key[V[(opcode & 0x0F00) >> 8]] == 0)
                         pc += 4;
                     else
@@ -268,18 +267,18 @@ void chip_8::emulateCycle() {
                     break;
 
                 default:
-                    printf("Unknown opcode [0xE000]: 0x%X\n", opcode);
+                    printf ("Unknown opcode [0xE000]: 0x%X\n", opcode);
             }
             break;
 
         case 0xF000:
             switch (opcode & 0x00FF) {
-                case 0x0007: // FX07: 将 VX 设置为延迟计时器的值
+                case 0x0007: // FX07: Sets VX to the value of the delay timer
                     V[(opcode & 0x0F00) >> 8] = delay_timer;
                     pc += 2;
                     break;
 
-                case 0x000A: { // FX0A: 等待按键,然后存储在 VX 中
+                case 0x000A: { // FX0A: A key press is awaited, and then stored in VX
                     bool keyPress = false;
 
                     for (int i = 0; i < 16; ++i) {
@@ -289,7 +288,7 @@ void chip_8::emulateCycle() {
                         }
                     }
 
-                    // 如果没有收到一个按键,跳过这个循环,然后再试一次
+                    // If we didn't received a keypress, skip this cycle and try again.
                     if (!keyPress)
                         return;
 
@@ -297,18 +296,18 @@ void chip_8::emulateCycle() {
                 }
                     break;
 
-                case 0x0015: // FX15: 将延迟计时器设置为 VX
+                case 0x0015: // FX15: Sets the delay timer to VX
                     delay_timer = V[(opcode & 0x0F00) >> 8];
                     pc += 2;
                     break;
 
-                case 0x0018: // FX18: 将声音计时器设置为 VX
+                case 0x0018: // FX18: Sets the sound timer to VX
                     sound_timer = V[(opcode & 0x0F00) >> 8];
                     pc += 2;
                     break;
 
-                case 0x001E: // FX1E: I += VX
-                    if (I + V[(opcode & 0x0F00) >> 8] > 0xFFF)    // (I+VX>0xFFF) 时 VF 设置为 1 ,否则为 0
+                case 0x001E: // FX1E: Adds VX to I
+                    if (I + V[(opcode & 0x0F00) >> 8] > 0xFFF)	// VF is set to 1 when range overflow (I+VX>0xFFF), and 0 when there isn't.
                         V[0xF] = 1;
                     else
                         V[0xF] = 0;
@@ -316,45 +315,46 @@ void chip_8::emulateCycle() {
                     pc += 2;
                     break;
 
-                case 0x0029: // FX29: 将 I 设置为 VX 中角色的精灵位置
+                case 0x0029: // FX29: Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font
                     I = V[(opcode & 0x0F00) >> 8] * 0x5;
                     pc += 2;
                     break;
 
-                case 0x0033: // FX33: 存储 VX 的二进制编码十进制表示，最高有效位在 I 中的地址的三位数字，中间位在 I 加 1，最低有效位在 I 加 2
-                    memory[I] = V[(opcode & 0x0F00) >> 8] / 100;
+                case 0x0033: // FX33: Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2
+                    memory[I]     = V[(opcode & 0x0F00) >> 8] / 100;
                     memory[I + 1] = (V[(opcode & 0x0F00) >> 8] / 10) % 10;
                     memory[I + 2] = (V[(opcode & 0x0F00) >> 8] % 100) % 10;
                     pc += 2;
                     break;
-                case 0x0055: // FX55: 将 V0 到 VX（包括 VX）存储在内存中，从地址 I 开始。对于每个写入的值，从 I 的偏移量增加 1，但 I 本身保持不变
+
+                case 0x0055: // FX55: Stores V0 to VX in memory starting at address I
                     for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
                         memory[I + i] = V[i];
 
-                    // 指令执行结束后, I = I + X + 1.
+                    // On the original interpreter, when the operation is done, I = I + X + 1.
                     I += ((opcode & 0x0F00) >> 8) + 1;
                     pc += 2;
                     break;
 
-                case 0x0065: // FX65: 用内存中的值从 V0 填充到 VX（包括 VX），从地址 I 开始。对于每个读取的值，从 I 的偏移量增加 1，但 I 本身保持不变
+                case 0x0065: // FX65: Fills V0 to VX with values from memory starting at address I
                     for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
                         V[i] = memory[I + i];
 
-                    // 指令执行结束后, I = I + X + 1.
+                    // On the original interpreter, when the operation is done, I = I + X + 1.
                     I += ((opcode & 0x0F00) >> 8) + 1;
                     pc += 2;
                     break;
 
                 default:
-                    printf("Unknown opcode [0xF000]: 0x%X\n", opcode);
+                    printf ("Unknown opcode [0xF000]: 0x%X\n", opcode);
             }
             break;
 
         default:
-            printf("Unknown opcode: 0x%X\n", opcode);
+            printf ("Unknown opcode: 0x%X\n", opcode);
     }
 
-    // 更新计时器
+    // Update timers
     if (delay_timer > 0)
         --delay_timer;
 
